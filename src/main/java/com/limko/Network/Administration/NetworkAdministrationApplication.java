371810d35d83a82bc45d4f42173b40026a28a354
wandile <nanashongwe@gmail.com>
2025-01-24 13:00:59 +0200

    module started

---
+++ src/main/java/com/limko/Network/Administration/NetworkAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.limko.Network.Administration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkAdministrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/limko/Network/Administration/NetworkAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limko.Network.Administration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkAdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
